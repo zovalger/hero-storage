c50baa5015451cdb6656e821ede745aebbb52f14
CesarEMB <cesarmb26@gmail.com>
2023-04-23 00:05:30 -0400

    nav(sin completar)

---
+++ src/pages/registro.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import style from "../styles/regi_login/style.module.css"
+
+export default function Registro(){
+
+    return(
+        <div className={style.container}>
+
+            <div></div>
+
+        </div>
+    )
+}
